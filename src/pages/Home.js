4fc826404277a867186627c079a654be6697c0b2
Germano Alves <germanomacedoalves@hotmail.com>
2022-04-25 01:20:55 -0300

    version 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 /* eslint-disable array-callback-return */
 import React, { useState, useEffect } from "react";
 import fireDb from "../firebase";
-import firebase from "../firebase";
-import { useLocation, Link } from "react-router-dom";
+//import firebase from "../firebase";
+//import { useLocation, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import { toast } from "react-toastify";
 import moment from "moment";
 import "moment/locale/pt-br";
-import Button from "@mui/material/Button";
-import CurrencyFormat from "react-currency-format";
+//import Button from "@mui/material/Button";
+//import CurrencyFormat from "react-currency-format";
 import { Despesa } from "./Despesa";
 import { Receita } from "./Receita";
 import { Total } from "./Total";
@@ -20,6 +21,7 @@ export const Home = () => {
   const [data, setData] = useState({});
   const [sortedData, setSortedData] = useState([]);
   const [sort, setSort] = useState(false);
+  const [style, setStyle] = useState("show");
 
   const [bla, setBla] = useState({
     despesa: undefined,
@@ -67,7 +69,7 @@ export const Home = () => {
         if (err) {
           toast.error(err);
         } else {
-          toast.success("Contato excluído com sucesso!");
+          toast.success("Conta excluída com sucesso!");
         }
       });
     }
@@ -87,6 +89,9 @@ export const Home = () => {
     fireDb.child("contacts").on("value", (snapshot) => {
       if (snapshot.val() !== null) {
         setData({ ...snapshot.val() });
+        let codeBlock = "";
+        document.getElementById("wrapper").innerHTML = codeBlock;
+        setStyle("show");
       } else {
         setData({});
       }
@@ -106,9 +111,50 @@ export const Home = () => {
       });
   };
 
+/* ------------------- start filter page */
+let dateInitial = new Date();
+let firstDay = new Date(dateInitial.getFullYear(), dateInitial.getMonth(), 1);
+let lastDay = new Date(
+  dateInitial.getFullYear(),
+  dateInitial.getMonth() + 1,
+  0
+);
+let defaultValue = new Date(firstDay).toISOString().split("T")[0];
+let defaultValueE = new Date(lastDay).toISOString().split("T")[0];
+
+//var fistFullDate = firstDay.getDate() + "-" + ((firstDay.getMonth() + 1)) + "-" + (firstDay.getFullYear());
+
+const [startDate1, setStartDate1] = useState(defaultValue);
+const [endDate1, setEndDate1] = useState(defaultValueE);
+const filterDate1 = (value) => {
+const startDateFilter1 = startDate1;
+const endDateFilter1 = endDate1;
+
+  fireDb
+    .child("contacts")
+    .orderByChild("data")
+    .startAt(startDateFilter1)
+    .endAt(endDateFilter1)
+    .on("value", (snapshot) => {
+      if (snapshot.val()) {
+        const data1 = snapshot.val();
+        setData(data1);
+      }
+    });
+};
+useEffect(() => {
+  filterDate1();
+}, []);
+
+/* ------------------------------------------*/
+
+
+
   /* -----------date fuction--------------*/
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+
+  // montar um useEffect com o primeiro e ultimo dia do mes processando a mesma filterDate
 
   const filterDate = (value) => {
     const startDateFilter = startDate;
@@ -120,14 +166,32 @@ export const Home = () => {
       .startAt(startDateFilter)
       .endAt(endDateFilter)
       .on("value", (snapshot) => {
-        if (snapshot.val()) {
+        if (snapshot.val() == null) {
+          const data = [];
+          setData(data);
+          if (setData(data) !== null) {
+            let codeBlock =
+              '<div class="content">' +
+              "<h1>Sem Valores</h1>" +
+              '<p>Clique no botão "Resetar Lista"</p>' +
+              "</div>";
+            document.getElementById("wrapper").innerHTML = codeBlock;
+            setStyle("hiddenTable");
+          }
+        } else {
+          let codeBlock = [];
           const data = snapshot.val();
           setData(data);
         }
       });
-    setStartDate({ startDate: "" });
-    setEndDate({ endDate: "" });
+    // setStartDate({ startDate: defaultValue });
+    // setEndDate({ endDate: defaultValueE });
+    //https://levelup.gitconnected.com/different-ways-to-check-if-an-object-is-empty-in-javascript-e1252d1c0b34
+    //https://stackoverflow.com/questions/66248378/how-to-map-an-object-keys-array-inside-an-object-keys-in-react-js
+    
   };
+
+  
 
   /* -----------sum fuction list--------------*/
 
@@ -140,17 +204,17 @@ export const Home = () => {
 
   /*--ranger datas campos de filtro */
 
-  let someDate = new Date();
-  let date = someDate.setDate(someDate.getDate());
-  let defaultValue = new Date(date).toISOString().split("T")[0];
-  /* ------- end date */
+  // let someDate = new Date();
+  // let date = someDate.setDate(someDate.getDate());
+  // let defaultValue = new Date(date).toISOString().split("T")[0];
+  // /* ------- end date */
 
-  let someDateE = new Date();
-  let numberOfDaysToAdd = 15;
-  let dateE = someDateE.setDate(someDateE.getDate() + numberOfDaysToAdd);
-  let defaultValueE = new Date(dateE).toISOString().split("T")[0];
+  // let someDateE = new Date();
+  // let numberOfDaysToAdd = 15;
+  // let dateE = someDateE.setDate(someDateE.getDate() + numberOfDaysToAdd);
+  // let defaultValueE = new Date(dateE).toISOString().split("T")[0];
 
-  /*-----------dados para o mapa---------- */
+  /*-----------dados para o gráfico---------- */
   let dadoNome = [];
   let dadoValor = [];
   Object.keys(data).map((id, index) => {
@@ -175,24 +239,26 @@ export const Home = () => {
           </button>
           <div className="mx-1 ">
             <input
+              id="startDate"
               type="date"
-              id="dateRequired"
-              name="dateRequired"
               defaultValue={defaultValue}
               className="max-w-xs"
-              value={startDate.startDate}
+              //value={startDate.startDate}
+
+              // onChange={console.log("vai", someDate)}
               onChange={(e) => setStartDate(e.target.value)}
-            ></input>
+            />
           </div>
           <div className="mx-1">
             <input
+              id="endDate"
               type="date"
-              id="dateRequired"
-              name="dateRequired"
               defaultValue={defaultValueE}
               className="max-w-xs"
-              value={endDate.endDate}
-              onChange={(e) => setEndDate(e.target.value)}
+              //value={endDate.endDate}
+              // usar um if pra isso aqui onChange={(e) => setEndDate(e.target.value)}
+              //https://pt.stackoverflow.com/questions/226086/como-retornar-o-primeiro-e-%C3%BAltimo-dia-do-m%C3%AAs-corrente-em-javascript
+               onChange={(e) => setEndDate(e.target.value)}
             ></input>
           </div>
           <button
@@ -352,76 +418,61 @@ export const Home = () => {
       <br />
       <div className="inline-grid grid-cols-2 gap-4">
         <div>
-          <table className="table-auto m-auto text-base shadow-xl rounded-lg dark:text-white dark:border-white dark:border">
-            <thead>
-              <tr className=" h-12 text-left border-b-2 border-gray-400 bg-gray-300 dark:text-black">
-                <th style={{ textAlign: "center" }}>Nº</th>
-                <th style={{ textAlign: "center" }}>Nome</th>
-                <th style={{ textAlign: "center" }}>Valor</th>
-                <th style={{ textAlign: "center" }}>Data</th>
-                <th style={{ textAlign: "center" }}>Categoria</th>
-                {!sort && <th style={{ textAlign: "center" }}>Action</th>}
-              </tr>
-            </thead>
-            {!sort && (
-              <tbody>
-                {Object.keys(data).map((id, index) => {
-                  return (
-                    <tr className=" border-b border-gray-300" key={id}>
-                      <th className="py-6 px-6" scope="row">
-                        {index + 1}
-                      </th>
-                      <td className="py-6 px-6">{data[id].name}</td>
-                      <td className="py-6 px-6" id="valores">
-                        {parseFloat(data[id].valor).toLocaleString("pt-br", {
-                          style: "currency",
-                          currency: "BRL",
-                        })}
-                      </td>
-                      <td className="py-6 px-6">
-                        {moment(data[id].data).format("L")}
-                      </td>
-                      <td className="py-6 px-6">{data[id].status}</td>
-                      <td className="py-6 px-6">
-                        <Link to={`/update/${id}`}>
-                          <button className="btn btn-edit">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                              />
-                            </svg>
-                          </button>
-                        </Link>
-                        <button
-                          className="btn btn-delete"
-                          onClick={() => onDelete(id)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
+          <div id="wrapper"></div>
+          <div className={style}>
+            <table className="table-auto m-auto text-base shadow-xl rounded-lg dark:text-white dark:border-white dark:border">
+              <thead>
+                <tr className=" h-12 text-left border-b-2 border-gray-400 bg-gray-300 dark:text-black">
+                  <th style={{ textAlign: "center" }}>Nº</th>
+                  <th style={{ textAlign: "center" }}>Nome</th>
+                  <th style={{ textAlign: "center" }}>Valor</th>
+                  <th style={{ textAlign: "center" }}>Data</th>
+                  <th style={{ textAlign: "center" }}>Categoria</th>
+                  {!sort && <th style={{ textAlign: "center" }}>Action</th>}
+                </tr>
+              </thead>
+              {!sort && (
+                <tbody>
+                  {Object.keys(data).map((id, index) => {
+                    return (
+                      <tr className=" border-b border-gray-300" key={id}>
+                        <th className="py-6 px-6" scope="row">
+                          {index + 1}
+                        </th>
+                        <td className="py-6 px-6">{data[id].name}</td>
+                        <td className="py-6 px-6" id="valores">
+                          {parseFloat(data[id].valor).toLocaleString("pt-br", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </td>
+                        <td className="py-6 px-6">
+                          {moment(data[id].data).format("L")}
+                        </td>
+                        <td className="py-6 px-6">{data[id].status}</td>
+                        <td className="py-6 px-6">
+                          <Link to={`/update/${id}`}>
+                            <button className="btn btn-edit">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                />
+                              </svg>
+                            </button>
+                          </Link>
+                          <button
+                            className="btn btn-delete"
+                            onClick={() => onDelete(id)}
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                            />
-                          </svg>
-                        </button>
-                        <Link to={`/view/${id}`}>
-                          <button className="btn btn-view">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               className="h-6 w-6"
@@ -433,18 +484,35 @@ export const Home = () => {
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 strokeWidth="2"
-                                d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                               />
                             </svg>
                           </button>
-                        </Link>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            )}
-            {/* {sort && (
+                          <Link to={`/view/${id}`}>
+                            <button className="btn btn-view">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                              </svg>
+                            </button>
+                          </Link>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              )}
+              {/* {sort && (
           <tbody>
             {sortedData.map((item, index) => {
               return (
@@ -459,7 +527,8 @@ export const Home = () => {
             })}
           </tbody>
         )} */}
-          </table>
+            </table>
+          </div>
         </div>
         <div className="flex justify-center text-center">
           <div className="">
